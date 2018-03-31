2008b69c397e50d39aacd332bbfb1ed62c09be83
Ahmed Awwad <sherman6@cs.washington.edu>
2018-03-31 15:04:36 -0400

    added docket bean

--- src/main/java/com/revature/hydra/bucket/BucketServiceApplication.java
+++ src/main/java/com/revature/hydra/bucket/BucketServiceApplication.java
@@ -4,8 +4,13 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 
+import springfox.documentation.builders.PathSelectors;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
 import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 
@@ -22,4 +27,14 @@ public class BucketServiceApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(BucketServiceApplication.class, args);
 	}
+	
+	@Bean
+    public Docket api() { 
+        return new Docket(DocumentationType.SWAGGER_2)  
+          .select()                                  
+          .apis(RequestHandlerSelectors.any())              
+          .paths(PathSelectors.any())                          
+          .build();                                           
+    }
+	
 }
